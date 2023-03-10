0f9dcac4c94fd4a232616871a0aa2783092637f8
Carlos Teixeira <101679144+HalterDevTi@users.noreply.github.com>
2023-03-10 10:20:22 -0300

    Add files via upload

---
+++ curso/VarLet.js
@@ -0,0 +1,10 @@
+/* Let não permite ser redeclarada! Var sim!*/
+var nome = "luiz"
+var nome = "carlos"
+console.log(nome);
+/* O terminal exibirá o último valor atribuído a Var!, no caso "Carlos*/
+let nome = "luiz"
+let nome = "carlos"
+console.log(nome);
+//resultará em erro!
+
